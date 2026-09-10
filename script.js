b18232dd314198b303b8075af95cc9cb4b53b239
Alex <alexwladymir17@gmail.com>
2026-09-10 18:07:14 -0500

    Agrega decoraciones romanticas

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ window.addEventListener("scroll", mostrarElementos);
 
 mostrarElementos();
 
-
 // ========================================
 // CORAZONES
 // ========================================
